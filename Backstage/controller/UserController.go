a7eeae31472bc355befdf0322a8b3492f5fb8f1c
zhanmmeng <1515341432@qq.com>
2022-05-20 13:44:01 +0800

    用户登录

--- Backstage/controller/UserController.go
+++ Backstage/controller/UserController.go
@@ -6,6 +6,7 @@ import (
 	"dora/Backstage/util"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
 	_ "math/rand"
 	"net/http"
 	_ "time"
@@ -39,18 +40,61 @@ func Register(c *gin.Context) {
 		return
 	}
 
+	//密码加密
+	hasedPassword, err := bcrypt.GenerateFromPassword([]byte(password),bcrypt.DefaultCost)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError,gin.H{"code":500,"msg":"加密错误"})
+		return
+	}
+
 	//创建
 	newUser := model.User{
 		Name:     name,
 		Phone:    phone,
-		Password: password,
+		Password: string(hasedPassword),
 	}
 	DB.Create(&newUser)
 
-	c.JSON(200, gin.H{"msg": "注册成功"})
+	c.JSON(200, gin.H{"code": 200, "msg": "注册成功"})
 }
 
+func Login(c *gin.Context) {
+	DB := common.GetDB()
 
+	//获取参数
+	password := c.PostForm("password")
+	phone := c.PostForm("phone")
+
+	//数据验证
+	if len(phone) != 11 {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": "422", "msg": "手机号必须为11位"})
+		return
+	}
+
+	if len(password) < 6 {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": "422", "msg": "密码不能少于6位"})
+		return
+	}
+
+	//手机号是否存在
+	var user model.User
+	DB.Where("phone = ?", phone).First(&user)
+	if user.ID == 0 {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": "422", "msg": "用户不存在"})
+		return
+	}
+
+	//判断密码是否正确
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password),[]byte(password)); err != nil{
+		c.JSON(http.StatusBadRequest, gin.H{"code": "400", "msg": "密码错误"})
+		return
+	}
+
+	//发放token
+	token := "11"
+
+	c.JSON(200, gin.H{"code": 200, "msg": "登录成功","data":gin.H{"token":token}})
+}
 
 func isTelephoneExist(db *gorm.DB, phone string) bool {
 	var user model.User
--- Backstage/router/router.go
+++ Backstage/router/router.go
@@ -8,6 +8,9 @@ import (
 func CollectRouter(r *gin.Engine) *gin.Engine  {
 	// 2.绑定路由规则，执行的函数
 	// gin.Context，封装了request和response
-	r.POST("/api/login", controller.Register)
+	r.POST("/api/register", controller.Register)
+
+	r.POST("/api/login", controller.Login)
+
 	return r
 }
